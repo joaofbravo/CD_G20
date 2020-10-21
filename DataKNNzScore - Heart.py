a419f30d3aa3b047156003521c6ff8fbfad8d664
Joao Bernardo <44246452+joaob123@users.noreply.github.com>
2020-10-21 03:32:31 +0100

    KNN & Bayes - Heart
    *.py files and *.png images for KNN and Naive Bayes for normal data and scalled data (zscore and minmax)

---
+++ DataKNNzScore - Heart.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import sklearn.metrics as metrics
+from sklearn.neighbors import KNeighborsClassifier
+import ds_functions as ds
+from ScalingHeart import scale_heart
+
+# KNN - zScore
+data: pd.DataFrame = pd.read_csv('data/heart_failure_clinical_records_dataset.csv')
+norm_data_zscore, norm_data_minmax = scale_heart(data)
+
+norm_data_zscore.pop('DEATH_EVENT')
+y: np.ndarray = data['DEATH_EVENT']
+X: np.ndarray = norm_data_zscore.values
+labels = pd.unique(y)
+
+trnX, tstX, trnY, tstY = train_test_split(X, y, train_size=0.7, stratify=y)
+
+nvalues = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+dist = ['manhattan', 'euclidean', 'chebyshev']
+values = {}
+best = (0, '')
+last_best = 0
+for d in dist:
+    yvalues = []
+    for n in nvalues:
+        knn = KNeighborsClassifier(n_neighbors=n, metric=d)
+        knn.fit(trnX, trnY)
+        prdY = knn.predict(tstX)
+        yvalues.append(metrics.accuracy_score(tstY, prdY))
+        if yvalues[-1] > last_best:
+            best = (n, d)
+            last_best = yvalues[-1]
+    values[d] = yvalues
+
+plt.figure()
+ds.multiple_line_chart(nvalues, values, title='KNN variants', xlabel='n', ylabel='accuracy', percentage=True)
+plt.show()
+print('Best results with %d neighbors and %s'%(best[0], best[1]))
+
+# Performance
+clf = knn = KNeighborsClassifier(n_neighbors=best[0], metric=best[1])
+clf.fit(trnX, trnY)
+prd_trn = clf.predict(trnX)
+prd_tst = clf.predict(tstX)
+ds.plot_evaluation_results(pd.unique(y), trnY, prd_trn, tstY, prd_tst)
+
